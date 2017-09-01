fd37d25872cc12ba61f0d5aabe99d4e92625a835
yyuueexxiinngg <yyuueexxiinngg@outlook.com>
2017-09-01 09:17:37 +0700

    Finish the binary convert form.

---
+++ Calculator/binaryConvert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class binaryConvert : Form
+    {
+        public binaryConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToInt32(richTextBox1.Text, 2).ToString();
+        }
+    }
+}
